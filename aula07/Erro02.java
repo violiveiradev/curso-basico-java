710130890fe7c1989ac58eb810ad14344e6c57fe
violiveiradev <violiveira.dev@gmail.com>
2021-09-06 12:46:05 -0300

    new file aula07

---
+++ aula07/Erro02.java
@@ -0,0 +1,12 @@
+public class Erro02 {
+
+    public static void main(String[] args) {
+        //var int cont; Erro
+        int cont = 0;
+
+        System.out.println("Você digitou: " + cont);
+
+    }
+
+
+}
---
+++ aula07/Erro03.java
@@ -0,0 +1,11 @@
+public class Erro03 {
+
+    //public static void Main(String[] args) Erro
+    public static void main(String[] args) {
+
+        System.out.println("Você digitou: " + 1/0);
+
+    }
+
+
+}
