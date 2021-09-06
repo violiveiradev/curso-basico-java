b5cd86b079d0faedfc76b793f7974e38062ebd93
violiveiradev <violiveira.dev@gmail.com>
2021-09-06 16:32:18 -0300

    new file aula11

---
+++ java-vscode/src/com/violiveiradev/cursojava/aula11/VariaveisBolean.java
@@ -0,0 +1,13 @@
+package com.violiveiradev.cursojava.aula11;
+
+public class VariaveisBolean {
+    public static void main(String[] args) {
+        
+        boolean verdadeiro = true;
+
+        boolean falso = false;
+
+        System.out.println("O valor de verdadeiro é " + verdadeiro);
+        System.out.println("O valor de falso é " + falso);
+    }
+}
---
+++ java-vscode/src/com/violiveiradev/cursojava/aula11/VariaveisPontoFlutuante.java
@@ -0,0 +1,15 @@
+package com.violiveiradev.cursojava.aula11;
+
+public class VariaveisPontoFlutuante {
+    public static void main(String[] args) {
+        
+        double valorPasagem = 2.90;
+
+        float valorTomate = 3.95f;
+
+        System.out.println("Valor da passagem = " + valorPasagem);
+        System.out.println("Valor do tomate = " + valorTomate);
+
+    }
+    
+}
