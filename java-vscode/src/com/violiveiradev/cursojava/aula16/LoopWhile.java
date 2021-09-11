86f96d4e6ea1dd30f8657266d89f9482c4ec6a19
violiveiradev <violiveira.dev@gmail.com>
2021-09-11 15:58:54 -0300

    Aula16

---
+++ java-vscode/src/com/violiveiradev/cursojava/aula16/LoopWhile.java
@@ -0,0 +1,24 @@
+package com.violiveiradev.cursojava.aula16;
+
+public class LoopWhile {
+    
+    public static void main(String[] args) {
+        
+        int i = 1;
+        int max = 10;
+
+        System.out.println("Contando até " + max);
+
+        while (i <= max) {
+            System.out.println("Valor de i: " + i);
+            i++; // i = i + 1; ou i +=1;
+        } 
+
+        do {
+            i++;
+            System.out.println("Valor de i: " + i);
+        } while (i < 15);
+
+        System.out.println(i);
+    }
+}
