6051f278f512f9c3dfaef582dd5c1cdb4e997040
violiveiradev <violiveira.dev@gmail.com>
2022-07-03 22:01:47 -0300

    Aplicando interfaces

--- bytebank-herdado/src/SistemaInterno.java
+++ bytebank-herdado/src/SistemaInterno.java
@@ -3,8 +3,9 @@ public class SistemaInterno {
 
     private int senha = 2222;
 
-    public void autentica(Gerente g) {
-        boolean autenticou = g.autentica(this.senha);
+    public void autentica(Autenticavel fa) {
+        boolean autenticou = fa.autentica(this.senha);
+
         if(autenticou) {
             System.out.println("Pode entrar no sistema!");
         } else {
